46320379cf5db8a1390f382ed4a51d04a5f34f8a
khayratdinov <khayratdinov.np@gmail.com>
2022-12-06 02:13:34 +0500

    chore:  🌥 add get weather function with Yandex Weater app

--- request.py
+++ request.py
@@ -9,7 +9,26 @@ def get_city_coord(city):
     payload = {"geocode": city, "apikey": config.geo_key, "format": "json"}
     r = requests.get("https://geocode-maps.yandex.ru/1.x", params=payload)
     geo = json.loads(r.text)
-    return print(geo)
+    return geo["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
+        "Point"
+    ]["pos"]
 
 
-get_city_coord("Toshkent")
+# get_city_coord("Toshkent")
+
+
+def get_weather(city):
+    coordinates = get_city_coord(city).split()
+    payload = {"lat": coordinates[1], "lon": coordinates[0], "lang": "ru_RU"}
+    r = requests.get(
+        "https://api.weather.yandex.ru/v2/informers",
+        params=payload,
+        headers=config.weather_key,
+    )
+    weather_data = json.loads(r.text)
+    return print(
+        weather_data["fact"],
+    )
+
+
+get_weather("Toshkent")
