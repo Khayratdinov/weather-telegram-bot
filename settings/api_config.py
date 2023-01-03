ddffb4fcdb578fe8cb7c69dffeba84e41265a924
Khayratdinov Nurpolat <Khayratdinov.np@gmail.com>
2023-01-03 14:02:00 +0500

    chore: 🕹 install aiogram

--- settings/api_config.py
+++ settings/api_config.py
@@ -1,4 +1,11 @@
+from sqlalchemy import create_engine
+
 geo_key = "d571e1c5-3443-4d05-a1de-33e105ffd4bf"
 
 
 weather_key = {"X-Yandex-API-Key": "e5f5f8fa-616f-42e4-8e4c-3950f5ddb1dc"}
+
+
+engine = create_engine('postgresql+psycopg2://postgres:admin12345@localhost:5432/botdb', echo=True)
+
+
